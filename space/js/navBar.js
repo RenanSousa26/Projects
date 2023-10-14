a54b4a501409adbe63ba8896d556830963b9aff7
Renan Pereira de Sousa <renansousa26it@gmail.com>
2023-10-14 18:24:43 -0300

    atualização 2

--- space/js/navBar.js
+++ space/js/navBar.js
@@ -1,17 +1,6 @@
-const links = document.getElementsByTagName('a');
+const links = document.querySelectorAll('a');
 const li = document.getElementsByTagName('li');
 const menuItem = document.querySelectorAll('.menu-item');
-
-
-for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener("mouseover", () => {
-        li[i].classList.add('hover-list');
-    });
-
-    links[i].addEventListener("mouseout", () => {
-        li[i].classList.remove('hover-list');
-    });
-}
 
 function selectLink(){
     menuItem.forEach((item) => 
