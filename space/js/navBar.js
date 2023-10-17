416493be86022a95f4d33ea931aa3d93c5550ec5
Renan Pereira de Sousa <renansousa26it@gmail.com>
2023-10-16 23:40:32 -0300

    navbar

--- space/js/navBar.js
+++ space/js/navBar.js
@@ -23,10 +23,18 @@ window.addEventListener("resize", function() {
         const mobileMenus = document.querySelector('.mobile-menu');
         const ul = document.querySelector('ul');
         const li = document.querySelectorAll('li');
+        const line1 = document.querySelector('#line1');
+        const line2 = document.querySelector('#line2');
+        const line3 = document.querySelector('#line3');
+        
         
 
         mobileMenus.addEventListener('click', () => {
             ul.classList.toggle('active-nav');
+            mobileMenus.classList.toggle('rotate');
+            line1.classList.toggle('ex1');
+            line2.classList.toggle('ex2');
+            line3.classList.toggle('ex3');
 
             li.forEach((item) => 
             item.classList.toggle('active-li')
